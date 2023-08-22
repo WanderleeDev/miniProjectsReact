c2fbccf3bf5a7293b22343d4c60df0c1adcf3f09
Wanderlee Max <wanderleedev@gmail.com>
2023-08-21 23:46:01 -0500

    Archivos base

---
+++ FecthBooks/src/App.tsx
@@ -0,0 +1,14 @@
+import {SearchBar} from './components/SearchBar'
+import { Card } from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <SearchBar />
+      <Card />
+    </>
+  )
+}
+
+export default App
